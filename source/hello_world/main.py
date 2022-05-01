da8dea45fb1536ec9c609e9c3da3c68b7a902a81
Luiz Paulo <luizpaulo13@gmail.com>
2022-05-01 10:13:00 -0300

    Added hello_world

--- source/hello_world/main.py
+++ source/hello_world/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 
-# Instantiate the App
+# Instantiate the app.
 app = FastAPI()
 
 # Define a GET on the specified endpoint
 @app.get("/")
 async def say_hello():
-    return {'greeting':"Hello World!"}
+    return {"greeting":"Hello World!"}
